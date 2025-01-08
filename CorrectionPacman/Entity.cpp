e08e7124c40602255becf6bca17dbffcc0bc75c4
konishiba <yoan.gimenez1202@gmail.com>
2025-01-08 18:05:45 +0100

    Delete CorrectionPacman directory

--- CorrectionPacman/Entity.cpp
+++
@@ -1,9 +0,0 @@
-#include "Entity.h"
-
-
-Entity::Entity(const string& _name, Vector2f _shapeSize)
-{
-	shapeSize = _shapeSize;
-	shape = RectangleShape(shapeSize);
-	TextureManager::GetInstance().InitShape(shape, texture,_name);
-}
